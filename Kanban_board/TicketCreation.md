b2dae3854aad67c6980019db21e46da0573a1a58
Jasbir <mr.jasbirsingh96@gmail.com>
2023-09-07 23:18:42 +0530

    lock ui feature

--- Kanban_board/TicketCreation.md
+++ Kanban_board/TicketCreation.md
@@ -1,8 +1,21 @@
 ## Steps
+## Get Data from Our Modal 
 [*] click on plus  button -> popup should appear
 [*] default selected color should be red 
 [*] we have apply the option to choose color of our ticket
 [] presses enter -> 
     [*] dialog box should disappear
-    [*] ticket is created 
-* Modal : you need to set it to default -> before making it visible 
+    [*] get required data for ticket  
+* Modal : you need to set it to default -> before making it visible
+
+## Creating ticket
+*  Creating the UI
+    [*] color strip
+    [*] id
+    [*] text
+    [*] lock button
+
+* lock button : able to toggle it
+    * locked -> non-editable
+    * unlocked : editable 
+            <!-- <i class="fa-solid fa-lock-open"></i> -->
--- Kanban_board/script.js
+++ Kanban_board/script.js
@@ -3,6 +3,9 @@ const modalContainer = document.querySelector(".modal-cont");
 const addBtn = document.querySelector(".add-btn");
 const colorModalArr = document.querySelectorAll(".color_modal");
 const textArea = document.querySelector(".textarea-cont")
+const mainContainer = document.querySelector("main");
+// varaible 
+const uid = new ShortUniqueId({ length: 5 });
 // add the event listener
 addBtn.addEventListener("click", function () {
     modalContainer.style.display = "flex";
@@ -35,9 +38,51 @@ textArea.addEventListener("keypress", function (event) {
         // currentcolor
         const currColorElem = modalContainer.querySelector(".selected");
         const taskColor = currColorElem.getAttribute("currColor");
-        console.log(task, taskColor);
-        // ->  reste your modal to default
+        // console.log(task, taskColor);
+        // ->  reset your modal to default
+        textArea.value = "";
+        createTicket(taskColor, task);
     }
 })
 
 
+function createTicket(taskColor, task) {
+    /*************added the UI of ticket************/
+    const id = uid.rnd();
+    const ticketContainer = document.createElement("div");
+    ticketContainer.setAttribute("class", "ticket-cont");
+    ticketContainer.innerHTML = `<div class="ticket-color ${taskColor}"></div>
+            <div class="ticket-id">#${id}</div>
+            <div class="ticket-area">${task}</div>
+             <i class="fa-solid fa-lock lock_icon"></i>
+            `;
+            
+    mainContainer.appendChild(ticketContainer);
+
+    /***  lock unclock feature */
+    const lockButton = ticketContainer.querySelector(".lock_icon");
+
+    handlelockButton(lockButton);
+
+}
+
+
+function handlelockButton(lockButton) {
+    lockButton.addEventListener("click", function () {
+        // Lock button : <i class="fa-solid fa-lock "></i>
+        // Unlock button:     < i class="fa-solid fa-lock-open" ></ >
+        const isLocked = lockButton.classList.contains("fa-lock");
+
+        if (isLocked == true) {
+            // have unlock it
+            lockButton.classList.remove("fa-lock");
+            lockButton.classList.add("fa-lock-open");
+        } else {
+            // lock it 
+            lockButton.classList.remove("fa-lock-open");
+            lockButton.classList.add("fa-lock");
+        }
+
+
+    })
+}
